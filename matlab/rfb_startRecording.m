13c2a93e64331b868e3938478e5b40f31218a06d
vincentvjj <vinc3nt_joe@yahoo.com>
2019-05-14 16:03:06 +0200

    editted more

--- matlab/rfb_startRecording.m
+++ matlab/rfb_startRecording.m
@@ -11,6 +11,9 @@ pyff('init',opt.feedback.name); pause(6);
 pyff('set',opt.feedback.pyff_params(id))
 
 basename = sprintf('%s_%s_',opt.session_name,opt.feedback.blocks{id});
+pyff('set','block_name',basename)
+pyff('set','data_dir',BTB.Tp.Dir)
+
 bbci_acquire_bv('close');
 pyff('play','basename',basename,'impedances',0);
 bbci_apply(bbci);
---
+++ matlab/rfb_startupBBCI.m
@@ -0,0 +1,5 @@
+
+cd('c:\bbci\bbci_public')
+startup_bbci_toolbox('DataDir', 'C:\Data')
+BTB.History = false;
+BTB.TypeChecking = false;
