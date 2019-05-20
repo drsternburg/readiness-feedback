9eeb66578bfd0c0bb36f48de14eec57578f5ce55
Vincent Jonany <vincentjonany@Vincents-MacBook-Pro-2.local>
2019-05-20 12:31:16 +0200

    polish code

--- ReadinessFeedback.py
+++ ReadinessFeedback.py
@@ -50,6 +50,7 @@ class ReadinessFeedback(PygameFeedback):
         self.white_color = [255,255,255]
         self.red_color = [225, 0, 0]
         self.radius = 25
+        self.cross_length = 25
  
         self.pause_text = 'Paused. Press pedal to start...'
         self.end_text = 'Finished. Session has ended...'
@@ -60,8 +61,8 @@ class ReadinessFeedback(PygameFeedback):
         ########################################################################
 
         self.marker_identifier = {
-            10:"Trial starts",
-            11:"Trial ends",
+            10:"Trial starts: " + self.trial_counter,
+            11:"Trial ends: " + self.block_counter,
             20:"block starts",
             21:"block ends",
             30:"feedback",
@@ -97,11 +98,7 @@ class ReadinessFeedback(PygameFeedback):
         self.mean_value = 50
         self.block_counter = 1
         
-        # self.rp_dist_init = [3.40158,5.91582,8.72199,2.09857,1.57752,2.13866,1.31238,0.6742,-0.860305,0.869217,-2.20397,-0.427483,3.09122,-3.15091,6.29236,3.96954,-2.99037,-0.372976,3.0263,3.8923,-0.655221,1.86118,2.16084,-0.00743317,-10.7251,0.493279,0.844872,2.12802,4.46754,2.9867,2.67655,4.06675,1.01152,2.13234,3.17019,-0.483233,2.92781,3.2214,-3.94281,-1.15404,4.38632,1.29367,4.01247,-0.0690076,6.65976,6.36116,-0.479293,6.05266,5.24286,4.2689,-3.3575,4.44705,1.55116,2.94615,2.08329,4.34001,2.62014,5.26946,1.24628,2.23645,1.19922,-0.454266,5.87512,4.72588,6.4719,6.14339,6.07847,8.75295,7.29186,5.12702,11.5874,1.30933,1.30272,-1.92392,-2.79681,0.776014,7.42855,0.952247,-0.469118,5.27124,3.45942,1.59118,3.96028,3.67294,3.03981,1.52358,2.41185,2.48132,2.03066,6.84007,4.25418,3.03598,2.84473,3.6167,1.53169,6.81807,2.31844,1.12883]
-        # self.rp_dist_init = sorted(self.rp_dist_init)
-        # self.mu = np.average(self.rp_dist_init)
-        # self.std = np.std(self.rp_dist_init)
-        
+       
     def pre_mainloop(self):
         PygameFeedback.pre_mainloop(self)
         self.font_text = pygame.font.Font(None, self.text_fontsize)
@@ -144,7 +141,6 @@ class ReadinessFeedback(PygameFeedback):
         self.draw_text(self.end_text)
 
     def unpause(self):
-        # self.log('Starting block ' + str(self.block_counter))
         self.send_parallel_log(self.marker_block_start) #Block starts here
         # Restart the history
         self.eeg_history = []
@@ -237,7 +233,7 @@ class ReadinessFeedback(PygameFeedback):
                         # write to file, the necessary info about the rp
                         content_to_write = self.log(
                             "Block: " + str(self.block_counter) +  
-                            "Trial: " + str(self.trial_counter) +
+                            " | Trial: " + str(self.trial_counter) +
                             " | " + str(rp) + 
                             " | " + str(rp_val_transformed) + 
                             " | " + self.eeg_history[index_emg_onset]['pyff_timestamp'][-12:] + 
@@ -318,10 +314,10 @@ class ReadinessFeedback(PygameFeedback):
 
     def draw_fixation_cross(self):
         self.screen.fill(self.background_color)
-        vertical_line = pygame.Surface((2, 25))
-        horizontal_line = pygame.Surface((25, 2))
-        self.screen.blit(horizontal_line, (self.screen_center[0] - (25/2), self.screen_center[1]))
-        self.screen.blit(vertical_line, (self.screen_center[0], self.screen_center[1] - (25/2)))
+        vertical_line = pygame.Surface((2, self.cross_length))
+        horizontal_line = pygame.Surface((self.cross_length, 2))
+        self.screen.blit(horizontal_line, (self.screen_center[0] - (self.cross_length/2), self.screen_center[1]))
+        self.screen.blit(vertical_line, (self.screen_center[0], self.screen_center[1] - (self.cross_length/2)))
         pygame.display.update()
         self.last_cross_shown = pygame.time.get_ticks()
 
